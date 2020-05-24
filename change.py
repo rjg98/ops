45a3a9940a22b1dfdb1fc79fa85f49bb3e476455
rjg98 <56362301+rjg98@users.noreply.github.com>
2020-05-24 17:26:45 +0530

    final change

---
+++ change.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[40]:
+
+
+k=open("MlOp.py",'r')
+a=k.readlines()
+k.close()
+count=open("data/COUNT.txt",'r')
+c=count.readlines()
+count.close()
+c
+
+
+# In[41]:
+
+
+if(c[0]=='1'):
+    with open ("MlOp.py",'w') as k:
+            for l in a:
+                if l.strip("\n")!="l(32,count,size)":
+                    k.write(l)
+                if l.strip("\n")=="l(32,count,size)":
+                    k.write(l.replace('l(32,count,size)','l(32,count,size)\nl(16,count,size)'))
+elif(c[0]=='2'):
+    with open ("MlOp.py",'w') as k:
+            for l in a:
+                if l.strip("\n")!="l(16,count,size)":
+                    k.write(l)
+                if l.strip("\n")=="l(16,count,size)":
+                    k.write(l.replace('l(16,count,3)','l(16,count,3)\nl(8,count,3)'))
+        
+
+
+# In[42]:
+
+
+print("model updated")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
